722d83b76e31abf761b631d151308daddcbd664d
Matthieu Napoli <matthieu@mnapoli.fr>
2016-11-19 18:51:37 +0100

    Remove Graphite stuff

--- run-graphite.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-docker run \
-  --rm \
-  --name graphite \
-  -p 8000:80 \
-  -p 2003:2003 \
-  -v $(pwd)/conf/storage-schemas.conf:/opt/graphite/conf/storage-schemas.conf \
-  sitespeedio/graphite
